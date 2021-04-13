ea8b297d692b758b3c8f1360297add0e201eb382
Giovanni <benyi_7r@hotmail.com>
2021-04-13 11:58:21 -0500

    se agrego las vistas de nuevo, editar y eliminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Persona } from './Modelo/Persona';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,20 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'proyecto01';
 
+  personas:Persona[];
+
   constructor(private router:Router){}
 
     Listar(){
       this.router.navigate(["listar"]);
     }
     Nuevo(){
+      this.router.navigate(["add"]);
+    }
+    Editar(){
+      this.router.navigate(["edit"]);
+    }
+    Eliminar(){
       this.router.navigate(["add"]);
     }
 
